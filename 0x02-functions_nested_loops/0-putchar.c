64933e4b147e2ea3a22cc2dde864af0e6ebf5186
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-03-21 11:08:56 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}

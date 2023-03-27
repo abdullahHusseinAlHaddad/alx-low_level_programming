27a1ddcf18bf9bf4a4d6d223aa0da7ef07a2343c
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-03-27 15:37:10 -0700

    a

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,11 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
 
-	while (s[index++])
+	while (s[len])
 		len++;
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	while (len--)
+		_putchar(s[len]);	
 	_putchar('\n');
 }

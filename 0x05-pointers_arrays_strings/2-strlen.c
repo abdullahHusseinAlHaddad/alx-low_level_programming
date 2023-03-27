157f609c754ed811dabf3effab6788fdc05b29e8
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-03-27 15:27:53 -0700

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s: The string to get the length of.
  *
  * Return: The length of @str.
  */
 int _strlen(char *s)
 {
-       int length = 0;
+	int length = 0;
 
-	for(length = 0; *s != '\0'; s++)
+	for (length = 0; *s != '\0'; s++)
 		++length;
 	return (length);
 }

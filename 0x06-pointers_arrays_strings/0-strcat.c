dae2ff924ee1a440bccbc009135ebcacd532a783
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-03-29 21:50:48 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,14 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int c2, c;
 
-	while (dest[desr_len])
-		dest_len++;
+	c = 0;
+	while (dest[c])
+		c++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
 
 	return (dest);
 }

73ce7afa8fa2b314ead21025b3e300b76d240dcb
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-04-05 17:21:35 -0700

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * sqrt - Find square root.
+ * @m: The number.
+ * @ro: The root.
+ * Return: number.
+ */
+
+int sqrt(int m, int ro)
+{
+	if ((ro * ro) == m)
+		return (ro);
+	else if (ro * ro < m)
+		return (sqrt(m, ro + 1));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - search for natural square root.
+ * @n: The number.
+ * Return: number.
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}

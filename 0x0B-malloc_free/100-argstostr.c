0bc7d42a6af001986469776ca3c80a7f7ee40678
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-04-11 21:04:41 -0700

    a

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - fuction.
+ * @ac: number.
+ * @av: An array.
+ * Return: new string.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *st;
+	int a = 0, xm = 0, k = 0, tp = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (; a < ac; a++, xm++)
+		xm += stln(av[0]);
+
+	st = malloc(sizeof(char) * xm + 1)
+		if (st == 0)
+			return (NULL);
+
+	for (a = 0; a < ac; a++)
+	{
+		for (k = 0; av[a][k]; k++, tp++)
+			st[tp] = av[a][k];
+		st[tp] = '\n';
+		tp++;
+	}
+	st[tp] = '\0';
+	return (st);
+}
+
+/**
+ * stln - search about length of string.
+ * @string: string.
+ * Return: integer
+ */
+
+int stln(char *string)
+{
+	int ze = 0;
+
+	for (; string[ze] != '\0'; ze++)
+		;
+	return (ze);
+}

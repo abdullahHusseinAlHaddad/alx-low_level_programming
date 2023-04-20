399313ea855193a7345bc9a58ae332a580d36202
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-04-20 06:11:32 -0700

    a

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,17 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		return;
 	}
 	va_start(str, n);
-	for (dex = 0; dex < n; dex++)
-	{
-		string = va_arg(str, char *);
-		if (string == NULL)
-			printf("(nil)");
-		else
-			printf("%s", string);
-
-		if (dex != (n - 1) && separator != NULL)
-			printf("%s", separator);
-	}
-	printf("\n");
+	while (dex--)
+		printf("%s%s", (string = va_arg(str, char *)) ? string : "(nil)",
+				dex ? (separator ? separator : " ") : "\n");
 	va_end(str);
 }

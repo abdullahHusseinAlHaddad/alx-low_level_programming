b0650f05a39b1fb5c48166565e142107fc14129d
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-02 17:53:21 -0700

    a

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print lists
+ * @h: pointer 
+ * Return: number.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		number++;
+	}
+	return (number);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - print lists
+ * @h: pointer
+ * Return: number.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		number++;
+		h = h->next;
+	}
+	return (number);
+}

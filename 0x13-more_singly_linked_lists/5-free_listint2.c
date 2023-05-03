2e6343d4d30b10ce0f112c9b96e28f07919cf62e
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-02 18:58:40 -0700

    a

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - free lists
+ * @head: pontr to pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (!head)
+		return;
+
+	while (te)
+	{
+		te = (*head)->next;
+		free(*head);
+		*head = te;
+	}
+	*head = NULL;
+}

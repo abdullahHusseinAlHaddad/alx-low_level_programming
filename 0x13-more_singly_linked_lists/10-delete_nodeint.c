0107d1c57767e69c5497f4afb7f69d5eed874901
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-02 23:06:18 -0700

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node, *pre;
 	unsigned int j = 0;
 
-	if (!head)
+	if (!head || !*head)
 		return (-1);
 	if (!index)
 	{

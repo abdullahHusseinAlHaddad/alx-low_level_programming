22997b03921aa2934489d5aebf65af90e5102db3
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-02 18:19:22 -0700

    a

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	new->next = NULL;
 	new->n = *n;
-	if(*head)
+	if (*head)
 		new->next = *head;
 	*head = new;
 	return (new);

d707b35de6eb33bf53775040f7d1720704c9cf09
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-04 19:49:51 -0700

    a

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
-		return (-1)
-	return (!!(*n |= 1L << index));
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n ^= (1 << index);
+	return (1);
 }

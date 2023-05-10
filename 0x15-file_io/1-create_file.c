807b250aa7873b803dfa5a70d24e69d2e64b4d49
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-10 06:13:35 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,19 +8,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int open, write, ln = 0;
+	int o, w, ln = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (ln = 0; text_content[ln];)
 			ln++;
 	}
-	open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write = write(open, text_content, ln);
-	if (open == -1 || write == -1)
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	close(open);
+	close(o);
 	return (1);
 }

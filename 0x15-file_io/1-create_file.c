c5bf0dc4514d7a111570552f7274c26a5b37d045
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-10 15:09:56 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,26 @@
 #include "main.h"
-/**
- * _str - return length.
- * @tr: string.
- * Return: number.
- */
-int _str(char *tr)
-{
-	int j = 0;
-
-	if (!tr)
-		return (0);
-	while (*tr++)
-		j++;
-	return (j);
-}
 
 /**
  * create_file - Creates a file.
  * @filename: A pointer.
  * @text_content: A pointer.
- * Return: number.
+ * Return: 1 or -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int on;
-	ssize_t byte = 0, ln = _str(text_content);
+	int fd, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	on = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_IWUSR);
-	if (on == -1)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
 		return (-1);
-	if (ln)
-		byte = write(on, text_content, ln);
-	close(on);
-	return (byte == ln ? 1 : -1);
+	close(fd);
+	return (1);
 }

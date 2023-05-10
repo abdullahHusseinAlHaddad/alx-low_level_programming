9b9c342e95dc7aec421c349472131fb1417046a5
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-05-10 06:06:13 -0700

    a

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer.
+ * @text_content: A pointer.
+ * Return: number.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int open, write, ln = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (ln = 0; text_content[ln];)
+			ln++;
+	}
+	open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write = write(open, text_content, ln);
+	if (open == -1 || write == -1)
+		return (-1);
+	close(open);
+	return (1);
+}

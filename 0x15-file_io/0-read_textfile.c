6307de2d4c2fa20e8143a98a7a800843236f6e6b
abdullahHusseinAlHaddad <abdullah.alhaddad392@gmail.com>
2023-10-02 09:52:05 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,17 +8,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t byte;
-	char buf[READ_BUF_SIZE * 8];
-	int f;
+	ssize_t o, r, w;
+	char *buffer;
 
-	if (!filename || !letters)
+	if (filename == NULL)
 		return (0);
-	f = open(filename, O_RDONLY);
-	if (f == -1)
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
-	byte = read(f, &buf[0], letters);
-	byte = write(STDOUT_FILEND, &buf[0], byte);
-	close(f);
-	return (byte);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
